1762006a24a8a75f688410148681efca8fe7bfa5
Muri-tuu <kenhopkins001@gmail.com>
2024-07-31 01:21:55 +0300

    FIT-TRACK APP

---
+++ src/utils/fetchData.js
@@ -0,0 +1,29 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_EXERCISE_API_KEY, // Correct environment variable
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_YOUTUBE_API_KEY, // Correct environment variable
+  },
+};
+
+export const fetchData = async (url, options) => {
+  try {
+    const res = await fetch(url, options);
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    throw error; // Re-throw the error to handle it in the calling code
+  }
+};
